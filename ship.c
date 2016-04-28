3ef0b7b1f1dc43ea7dd29c06a90f37db5554a589
Rena <renashinomiya@gmail.com>
2016-04-28 18:31:08 -0500

    Adding initial ship implementation

---
+++ ship.c
@@ -0,0 +1,69 @@
+/*=========================================================*//**
+ * @file ship.c
+ * @brief Implementation of Battleship ship methods
+ *//*=========================================================*/
+
+// This project
+#include "ship.h"
+
+/*============================================================*
+ * Ship length
+ *============================================================*/
+int ship_GetLength(Ship ship) {
+    // Get the ship length
+    
+    switch (ship) {
+        case CARRIER:
+            return 5;
+        
+        case BATTLESHIP:
+            return 4;
+        
+        case SUBMARINE:
+        case CRUISER:
+            return 3;
+        
+        case DESTROYER:
+            return 2;
+        
+        // Never happens
+        default:
+            return -1;
+    }
+    
+    // Never get here
+    return -1;
+}
+
+/*============================================================*
+ * Ship name
+ *============================================================*/
+const char *ship_GetLength(Ship ship) {
+    // Get the ship length
+    
+    switch (ship) {
+        case CARRIER:
+            return "Carrier";
+        
+        case BATTLESHIP:
+            return "Battleship";
+        
+        case SUBMARINE:
+            return "Submarine";
+        
+        case CRUISER:
+            return "Cruiser";
+        
+        case DESTROYER:
+            return "Destroyer";
+        
+        // Never happens
+        default:
+            return NULL;
+    }
+    
+    // Never get here
+    return NULL;
+}
+ 
+/*============================================================*/
