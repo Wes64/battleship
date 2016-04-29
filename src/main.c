40d6d9ad15463f6a03cbec1363d9b178ad475d56
Rena <renashinomiya@gmail.com>
2016-04-28 23:00:51 -0500

    Adding placeholder main function

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/*=========================================================*//**
+ * @file main.c
+ * @brief Main application for Battleship program
+ *//*=========================================================*/
+
+// Standard library
+#include <stdio.h>
+
+// This project
+#include "audrey.h"
+#include "field.h"
+
+/*============================================================*
+ * @brief Main function
+ *============================================================*/
+int main(int argc, char *argv[]) {
+    // Main function
+    
+    // TODO parse arguments
+    
+    // Done
+    return 0;
+}
+
+/*============================================================*/
