3f0d6ea5e7e2775af4d274e5ffb17e7e38a8b05b
Rena <renashinomiya@gmail.com>
2017-04-03 15:16:11 -0500

    Refactor AI to only manipulate a field

--- src/ai.c
+++ src/ai.c
@@ -5,181 +5,157 @@
  **************************************************************/
 
 // Standard library
-#include <stdio.h>      // fprintf, stderr
 #include <stdbool.h>    // bool
 #include <limits.h>     // INT_MAX
+#include <math.h>		// log2
 
 // This project
 #include "debug.h"      // eprintf, assert
-#include "ai.h"         // AI
-#include "ship.h"       // SHIP, N_SHIPS
 #include "field.h"      // FIELD
+#include "ai.h"         // AI
 
-/*============================================================*
- * Initialization
- *============================================================*/
-void ai_Create(AI *ai) {
-    
-    // Set up the field
-    field_Clear(&ai->field);
-    
-    // Initialize turn count
-    ai->turns = 0;
-    for (int i = 0; i < N_SHIPS; i++) {
-        ai->sink_turn[i] = 0;
+/**********************************************************//**
+ * @brief Get the length of the longest ship remaining.
+ * @param field: The field to check.
+ * @return The longest ship remaining on the field.
+ **************************************************************/
+static inline void ai_GetMinimumLength(const FIELD *field, int *full, int *partial) {
+	// Get the minimum length remaining
+    int lengthMin = INT_MAX;
+    int partialMin = INT_MAX;
+    for (SHIP ship = 0; ship < N_SHIPS; ship++) {
+		// Check if the ship is actually afloat.
+		int health = field_GetShipHealth(field, ship);
+        if (health > 0) {
+            // Get the minimum fragment size if the ship is hit.
+            int length = field_GetShipLength(ship);
+            if (health < length) {
+				// Smallest fragment is when everything is in
+				// the middle of the ship.
+				int fragmentMin = (int)log2(length - health);
+				if (fragmentMin == 0) {
+					fragmentMin = 1;
+				}
+				
+				// Update minimum length
+				if (fragmentMin < partialMin) {
+					partialMin = fragmentMin;
+				}
+			} else if (length < lengthMin) {
+				// An unhit ship remains
+				lengthMin = length;
+				if (lengthMin < partialMin) {
+					partialMin = lengthMin;
+				}
+			}
+        }
     }
+    
+    // Output parameters
+    assert(partialMin <= lengthMin);
+    if (full) {
+		*full = lengthMin;
+	}
+	if (partial) {
+		*partial = partialMin;
+	}
 }
 
 /*============================================================*
  * Turn chooser
  *============================================================*/
-bool ai_PlayTurn(AI *ai) {
-    
-    // Get the maximum length remaining
-    int length_min = INT_MAX;
-    int temp;
-    int length;
-    int health;
-    for (SHIP ship_id = 0; ship_id < N_SHIPS; ship_id++) {
-        if ((health = ai->field.ship_health[ship_id]) > 0) {
-            length = ship_GetLength(ship_id);
-            temp = (length == health)? length: ((length - health)/2);
-            length_min = (temp < length_min)? temp: length_min;
-        }
-    }
+bool ai_PlayTurn(FIELD *field) {
+    int fullMin;
+    int partialMin;
+    ai_GetMinimumLength(field, &fullMin, &partialMin);
     
     // Get probability maximum
-    int prob_max = -1;
-    int prob_x = -1;
-    int prob_y = -1;
+    int probabilityMax = -1;
+    int tileX = -1;
+    int tileY = -1;
     for (int x = 0; x < FIELD_SIZE; x++) {
         for (int y = 0; y < FIELD_SIZE; y++) {
-            // Already tried
-            if (ai->field.entry[x][y].status != UNTRIED) {
-                continue;
-            }
-
-            // Calculate the probability here (all guaranteed >= 1 if UNTRIED)
-            int view_left  = field_GetExtent(&ai->field, LEFT,  x, y, UNTRIED);
-            int view_right = field_GetExtent(&ai->field, RIGHT, x, y, UNTRIED);
-            int view_up    = field_GetExtent(&ai->field, UP,    x, y, UNTRIED);
-            int view_down  = field_GetExtent(&ai->field, DOWN,  x, y, UNTRIED);
-            assert(view_left  >= 1);
-            assert(view_right >= 1);
-            assert(view_up    >= 1);
-            assert(view_down  >= 1);
-
-            // Calculate the ai->field hits nearby
-            int near_horizontal = 0, near_vertical = 0, temp;
-            if ((temp = field_GetExtent(&ai->field, LEFT, x-1, y, HIT)) > 0) {
-                near_horizontal += temp;
-            }
-            if ((temp = field_GetExtent(&ai->field, RIGHT, x+1, y, HIT)) > 0) {
-                near_horizontal += temp;
-            }
-            if ((temp = field_GetExtent(&ai->field, UP, x, y-1, HIT)) > 0) {
-                near_vertical += temp;
-            }
-            if ((temp = field_GetExtent(&ai->field, DOWN, x, y+1, HIT)) > 0) {
-                near_vertical += temp;
-            }
-            assert(near_horizontal >= 0);
-            assert(near_vertical >= 0);
-            
-            // Blocked
-            int blocked_horizontal = (view_right+view_left-1) < (length_min-near_horizontal);
-            int blocked_vertical = (view_up+view_down-1) < (length_min-near_vertical);
-            
-            // View is blocked when total distance is less than the length of the
-            // smallest ship
-            int prob;
-            if (blocked_horizontal && blocked_vertical) {
-                prob = 0;
-            } else {
-                prob = (view_left*view_right) + (view_up*view_down);
-                // Nearness weight
-                prob += (near_horizontal+near_vertical)*FIELD_SIZE*FIELD_SIZE;
-                assert(prob > 0);
-            }
-            assert(prob >= 0);
+			// Already tried
+			if (field_GetStatus(field, x, y) != UNTRIED) {
+				continue;
+			}
+			
+			// Calculate view extents from the current tile
+			// These are all guaranteed >= 1 if the tile is UNTRIED.
+			int viewLeft  = field_GetExtent(field, LEFT,  x, y, UNTRIED);
+			int viewRight = field_GetExtent(field, RIGHT, x, y, UNTRIED);
+			int viewUp    = field_GetExtent(field, UP,    x, y, UNTRIED);
+			int viewDown  = field_GetExtent(field, DOWN,  x, y, UNTRIED);
+			assert(viewLeft >= 1);
+			assert(viewRight >= 1);
+			assert(viewUp >= 1);
+			assert(viewDown >= 1);
+			
+			// Find any nearby hits, beginning at our neighbors
+			// and extending outwards.
+			int nearLeft  = field_GetExtent(field, LEFT,  x-1, y,   HIT);
+			int nearRight = field_GetExtent(field, RIGHT, x+1, y,   HIT);
+			int nearUp    = field_GetExtent(field, UP,    x,   y-1, HIT);
+			int nearDown  = field_GetExtent(field, DOWN,  x,   y+1, HIT);
+			int nearHorizontal = nearLeft + nearRight;
+			int nearVertical = nearUp + nearDown;
+			assert(nearLeft >= 0);
+			assert(nearRight >= 0);
+			assert(nearUp >= 0);
+			assert(nearDown >= 0);
+			
+			// If we are actually next to a hit, we need to use the partial
+			// minimum. Otherwise we aren't next to a hit and it is safe to
+			// use the full ship minimum.
+			bool blockedHorizontal;
+			if (nearLeft > 0 || nearRight > 0) {
+				blockedHorizontal = (viewRight+viewLeft) <= (partialMin-nearHorizontal);
+			} else {
+				blockedHorizontal = (viewRight+viewLeft) <= (fullMin-nearHorizontal);
+			}
+			
+			// Vertical check corresponds exactly
+			bool blockedVertical;
+			if (nearUp > 0 || nearDown > 0) {
+				blockedVertical = (viewUp+viewDown) <= (partialMin-nearVertical);
+			} else {
+				blockedVertical = (viewUp+viewDown) <= (fullMin-nearVertical);
+			}
+			
+			// Determine the probability at the tile.
+			int probability = 0;
+			if (!blockedHorizontal || !blockedVertical) {
+				// Weight probability towards the center of the view.
+				probability = viewLeft*viewRight + viewUp*viewDown;
+				// Weight a lot if near to other hits.
+				probability += (nearHorizontal+nearVertical)*(FIELD_SIZE*FIELD_SIZE);
+			}
+			assert(probability >= 0);
             
             // Check probability maximum
-            if (prob > prob_max) {
-                prob_max = prob;
-                prob_x = x;
-                prob_y = y;
+            if (probability > probabilityMax) {
+                probabilityMax = probability;
+                tileX = x;
+                tileY = y;
             }
         }
     }
-    assert(prob_max >= 0);
-    assert(ai->field.entry[prob_x][prob_y].status == UNTRIED);
+    
+    // Sanity check before making the attack
+    assert(tileX != -1);
+    assert(tileY != -1);
+    assert(field_GetStatus(field, tileX, tileY) == UNTRIED);
 
     // Make attack
-    if (!field_Attack(&ai->field, prob_x, prob_y)) {
-        eprintf("Attack at (%d, %d) failed.\n", prob_x, prob_y);
+    STATUS result = field_Attack(field, tileX, tileY);
+    if (result == ERROR) {
+        eprintf("Attack at (%d, %d) failed.\n", tileX, tileY);
         return false;
     }
-    assert(ai->field.entry[prob_x][prob_y].status != UNTRIED);
     
-    // Advance turn
-    if (ai->turns > TURN_MAX) {
-        eprintf("Turn overflow\n");
-        return false;
-    }
-    ai->turns++;
-
-    // Get the turn sunk for newly sunk ships
-    if (ai->field.entry[prob_x][prob_y].status == SUNK) {
-        SHIP ship = ai->field.entry[prob_x][prob_y].ship;
-        assert(ship >= 0);
-        ai->sink_turn[ship] = ai->turns;
-    }
-    
-    // Done
-    return true;
-}
-
-/*============================================================*
- * Game driver
- *============================================================*/
-bool ai_Play(AI *ai, FILE *output) {
-    
-    // Print the ship configuration
-    if (output) {
-        fprintf(output, "===== SHIP configuration =====\n");
-        field_PrintShips(&ai->field, output);
-        fprintf(output, "\n");
-        fflush(output);
-    }
-    
-    // Execute one game
-    while (!field_Win(&ai->field)) {
-        // Get the current option
-        if (!ai_PlayTurn(ai)) {
-            eprintf("Failed to play turn.\n");
-            return false;
-        }
-        
-        // Output file
-        if (output) {
-            fprintf(output, "===== Turn %d status =====\n", ai->turns);
-            field_PrintStatus(&ai->field, output);
-            fprintf(output, "\n");
-            fflush(output);
-        }
-    }
-    
-    // Print the final stats
-    if (output) {
-        fprintf(output, "===== Final stats =====\n");
-        fprintf(output, "Turns: %d\n", ai->turns);
-        for (SHIP ship_id = 0; ship_id < N_SHIPS; ship_id++) {
-            fprintf(output, "%s sink turn: %d\n", ship_GetName(ship_id), ai->sink_turn[ship_id]);
-        }
-        fflush(output);
-    }
-    
-    // Success
+    // Sanity check after attacking
+    assert(field_GetStatus(field, tileX, tileY) != UNTRIED);
+    assert(field_GetStatus(field, tileX, tileY) == result);
     return true;
 }
 
--- src/ai.h
+++ src/ai.h
@@ -8,65 +8,18 @@
 #define _AI_H_
 
 // Standard library
-#include <stdio.h>      // FILE
 #include <stdbool.h>    // bool
+#include <stdio.h>      // FILE
 
 // This project
-#include "field.h"      // FIELD, FIELD_SIZE
-#include "ship.h"       // N_SHIPS
-
-/**********************************************************//**
- * @struct AI
- * @brief Struct for AI data.
- **************************************************************/
-typedef struct {
-    FIELD field;            ///< The field the AI is playing on
-    char turns;             ///< The turn count
-    char sink_turn[N_SHIPS];///< When each ship sank
-} AI;
-
-/// @brief The maximum number of turns
-#define TURN_MAX (FIELD_SIZE*FIELD_SIZE)
-
-/**********************************************************//**
- * @brief Initializes the AI parameters.
- * @param ai: The AI instance to initialize.
- **************************************************************/
-extern void ai_Create(AI *ai);
-
-/**********************************************************//**
- * @brief Get the turn count.
- * @param ai: The AI that plays the game.
- * @return The number of turns taken.
- **************************************************************/
-static inline int ai_GetTurnCount(const AI *ai) {
-    return ai->turns;
-}
-
-/**********************************************************//**
- * @brief Get the turn the given ship sank.
- * @param ai: The AI that plays the game.
- * @param ship: The identity of a ship.
- * @return The number of turns taken to sink the ship.
- **************************************************************/
-static inline int ai_GetSinkTurn(const AI *ai, SHIP ship) {
-    return ai->sink_turn[ship];
-}
-
-/**********************************************************//**
- * @brief Play an entire game.
- * @param ai: The AI that plays the game.
- * @param output: File to output game data to or NULL.
- * @return Whether the gameplay succeeded.
- **************************************************************/
-extern bool ai_Play(AI *ai, FILE *output);
+#include "field.h"      // FIELD
 
 /**********************************************************//**
  * @brief Play one turn of a game.
- * @param ai: The AI that plays the game.
+ * @param field: The field to take a turn on.
  * @return Whether the gameplay succeeded.
  **************************************************************/
-extern bool ai_PlayTurn(AI *ai);
+extern bool ai_PlayTurn(FIELD *field);
 
 /*============================================================*/
 #endif
